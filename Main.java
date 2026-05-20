1af4c4f669efa40d014ef5c34a2b6a4cea88877a
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-20 07:00:56 +0530

    feat:[leela sai kumar] starting code of program

---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
+class Node{
+    int data;
+    Node left;
+    Node right;
+    public Node(int data) {
+        this.data = data;
+    }
+    
+}
+class BinaryTree{
+    Node root;
+}
