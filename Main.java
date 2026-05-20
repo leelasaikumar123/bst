c035ddb5aa71d3710d156cf576c79e55b657c5f8
leelasaikumar123 <leelasaikumarmeda501@gmail.com>
2026-05-20 07:14:32 +0530

    feat:[leela sai kumar meda] creation of tree using 56 30 and 70

--- Main.java
+++ Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args) {
-        
+     BinaryTree tree=new BinaryTree();
+     tree.insert(56);
+     tree.insert(30);
+     tree.insert(70);   
+     System.out.println("Tree Has");
+     tree.print();
     }
 }
 class Node{
@@ -14,4 +19,33 @@ class Node{
 }
 class BinaryTree{
     Node root;
+    public void insert(int data){
+        root=insertRec(root, data);
+    }
+
+
+    public Node insertRec(Node root,int data){
+     if(root == null){
+        root=new Node(data);
+     }
+    else if(data<root.data){
+        root.left=insertRec(root.left,data);
+    }
+        else if(data>root.data){
+        root.right=insertRec(root.right,data);
+    }
+     return root;
+    }
+
+    public void print(){
+      printRec(root);
+    }
+
+    public void printRec(Node root){
+      if(root != null){
+        printRec(root.left);
+        System.out.print(root.data+" ");
+        printRec(root.right);
+      }
+    }
 }
